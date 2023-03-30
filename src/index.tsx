cdd3b5a5e0f40483c979ac6e0f70e76490ab3b69
Ksenia Tuzova <102681427+Ksenia-Tuzova2@users.noreply.github.com>
2023-03-31 00:02:03 +0300

    new details

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
-
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { store } from './store/redux-store';
 import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
+import { dividerClasses } from '@mui/material';
 
 
-    import { createRoot } from 'react-dom/client';
     const container = document.getElementById('root');
     const root = createRoot(container!); 
     root.render(<Provider store={store}><App /></Provider>);
+
 
 // If you want your app to work offline and load faster,
 // you can change
